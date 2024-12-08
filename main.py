e4f0a38741d3338a163c8ffdab23163cb1d8edf1
MaXIDoGG <maks.nasonov.03@gmail.com>
2024-12-08 13:01:40 +0700

    Added new files

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello world!")
+from telebot import TeleBot
+from constants import TOKEN
+from todo.controllers import Controller
+
+# Создаем экземпляр бота
+bot = TeleBot(TOKEN)
+controller = Controller(bot)
+
+# Подключаем обработчики
+controller.register_handlers()
+
+if __name__ == "__main__":
+    print("Bot is running...")
+    bot.polling()
